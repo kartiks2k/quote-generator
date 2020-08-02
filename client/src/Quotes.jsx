843567f4de0f612fc9c1c3385f8ab005ab6c703b
kartik sood <kartiks2k@gmail.com>
2020-08-02 20:50:59 +0530

    deployable v3

--- client/src/Quotes.jsx
+++ client/src/Quotes.jsx
@@ -6,7 +6,7 @@ function Quotes(){
   const [author,setAuthor]=useState("");
 
   function getQuote() {
-    axios.get("http://localhost:8000",  { crossdomain: true }).then(response => {
+    axios.get("/",  { crossdomain: true }).then(response => {
       const {text,author}=response.data;
       setText(text);
       setAuthor(author);
