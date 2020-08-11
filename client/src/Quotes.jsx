2af50a397fe78946c849795e97411ceb32061af7
kartik sood <kartiks2k@gmail.com>
2020-08-11 16:52:20 +0530

    copy functionlity and footer added

--- client/src/Quotes.jsx
+++ client/src/Quotes.jsx
@@ -1,9 +1,10 @@
-import React , {useState} from "react";
+import React , {useRef, useState} from "react";
 import axios from "axios";
 
 function Quotes(){
   const [text,setText]=useState("");
   const [author,setAuthor]=useState("");
+  const h1ref = useRef(null);
 
   function getQuote() {
     axios.get("/getQuote",  { crossdomain: true }).then(response => {
@@ -13,11 +14,27 @@ function Quotes(){
     });
   }
 
+  function copyText() {
+    var el = document.getElementsByClassName("quote")[0];
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+    document.execCommand("copy");
+    sel.removeAllRanges();
+  };
+
   return(
     <div>
-      <h1 className="quote">{text}</h1>
+      <h1 className="quote" ref={h1ref}>{text}</h1>
       <h3 className="name">{author}</h3>
       <button className="btn" onClick={getQuote}>Generate Quote</button>
+      <button class="btn" onClick={copyText}>
+        <span class="material-icons">
+          content_copy
+        </span>
+      </button>
     </div>
   )
 }
